027350d318d61f61bfc155eb580ec9cb4f27b0e6
firelemons <shenyang64@gmail.com>
2021-05-29 00:48:17 -0500

    refatored org data structure for scaling

--- js/index.js
+++ js/index.js
@@ -37,28 +37,34 @@ function makeDiff (stats) {
 const app = new Vue({
   el: '#app',
   data: {
-    orgs: {
-      'Prince George': {
-        'Accepted Invitations'                              : {savedValue: 0, newValue: 0},
-        'Unaccepted Invitations'                            : {savedValue: 0, newValue: 0},
-        'Cases With Mandates'                               : {savedValue: 0, newValue: 0},
-        'Case Contact Count'                                : {savedValue: 0, newValue: 0},
-        'Case Contact Count in Last 2 Weeks'                : {savedValue: 0, newValue: 0},
-        'Users Who Have Added Case Contacts in Last 2 Weeks': {savedValue: 0, newValue: 0},
-        'Notification Count'                                : {savedValue: 0, newValue: 0},
-        'Volunteers Assigned to Supervisors'                : {savedValue: 0, newValue: 0}
+    orgs: [
+      {
+        'name': 'Prince George',
+        'metrics' : {
+          'Accepted Invitations'                              : {savedValue: 0, newValue: 0},
+          'Unaccepted Invitations'                            : {savedValue: 0, newValue: 0},
+          'Cases With Mandates'                               : {savedValue: 0, newValue: 0},
+          'Case Contact Count'                                : {savedValue: 0, newValue: 0},
+          'Case Contact Count in Last 2 Weeks'                : {savedValue: 0, newValue: 0},
+          'Users Who Have Added Case Contacts in Last 2 Weeks': {savedValue: 0, newValue: 0},
+          'Notification Count'                                : {savedValue: 0, newValue: 0},
+          'Volunteers Assigned to Supervisors'                : {savedValue: 0, newValue: 0}
+        }
       }, 
-      'Montgomery': {
-        'Accepted Invitations'                              : {savedValue: 0, newValue: 0},
-        'Unaccepted Invitations'                            : {savedValue: 0, newValue: 0},
-        'Cases With Mandates'                               : {savedValue: 0, newValue: 0},
-        'Case Contact Count'                                : {savedValue: 0, newValue: 0},
-        'Case Contact Count in Last 2 Weeks'                : {savedValue: 0, newValue: 0},
-        'Users Who Have Added Case Contacts in Last 2 Weeks': {savedValue: 0, newValue: 0},
-        'Notification Count'                                : {savedValue: 0, newValue: 0},
-        'Volunteers Assigned to Supervisors'                : {savedValue: 0, newValue: 0}
+      {
+        'name': 'Montgomery',
+        'metrics': {
+          'Accepted Invitations'                              : {savedValue: 0, newValue: 0},
+          'Unaccepted Invitations'                            : {savedValue: 0, newValue: 0},
+          'Cases With Mandates'                               : {savedValue: 0, newValue: 0},
+          'Case Contact Count'                                : {savedValue: 0, newValue: 0},
+          'Case Contact Count in Last 2 Weeks'                : {savedValue: 0, newValue: 0},
+          'Users Who Have Added Case Contacts in Last 2 Weeks': {savedValue: 0, newValue: 0},
+          'Notification Count'                                : {savedValue: 0, newValue: 0},
+          'Volunteers Assigned to Supervisors'                : {savedValue: 0, newValue: 0}
+        }
       }
-    },
+    ],
     global: {
       'Total Hours in Case Contacts': { savedValue: 0, newValue: 0}
     },
@@ -71,7 +77,7 @@ const app = new Vue({
       'Users Who Have Added Case Contacts in Last 2 Weeks': 'unloaded',
       'Notification Count'                                : 'unloaded',
       'Total Hours in Case Contacts'                      : 'unloaded',
-      'Volunteers Assigned to Supervisors'                : 'unloaded'
+      'Volunteers AssignVed to Supervisors'               : 'unloaded'
     }
   },
   computed: {
@@ -82,17 +88,19 @@ const app = new Vue({
     },
 
     diffs: function () {
-      const PGDiff = makeDiff(this.orgs['Prince George'])
-      const MontgomeryDiff = makeDiff(this.orgs['Montgomery'])
+      const orgs = {}
+
+      this.orgs.forEach((org) => {
+        if (org) {
+          orgs[org.name] = makeDiff(org.metrics)
+        }
+      })
+
       const globalDiff = makeDiff(this.global)
 
       return {
-        orgs: {
-          'Prince George': PGDiff,
-          'Montgomery'   : MontgomeryDiff
-        },
-
-        global: globalDiff
+        'orgs': orgs,
+        'global': globalDiff
       }
     }
   },
@@ -100,139 +108,91 @@ const app = new Vue({
     download: function () {
       downloadToFile(this.backup, 'backup.js', 'text/plain');
     },
+
     save: function () {
-      [this.orgs['Prince George'], this.orgs['Montgomery'], this.global].forEach((stats) => {
-        for(stat in stats) {
-          stats[stat].savedValue = stats[stat].newValue
+      this.orgs.map((org) => org.metrics).concat(this.global).forEach((metrics) => {
+        for(metric in metrics) {
+          metrics[metric].savedValue = metrics[metric].newValue
         }
       })
 
       localStorage.setItem('metrics', JSON.stringify({orgs: this.orgs, global: this.global}))
+    },
+
+    updateMetric: function (name, owner, value) {
+      if (owner === 'global') {
+        this.global[name].newValue = Number.isInteger(value) ? value : Math.round(value)
+      } else {
+        this.orgs[owner][name].newValue = Number.isInteger(value) ? value : Math.round(value)
+      }
     }
   },
   mounted: function() {
     const savedData = JSON.parse(localStorage.getItem('metrics'))
 
     if (savedData) {
-      for (orgName in this.orgs) {
-        this.orgs[orgName] = Object.assign(this.orgs[orgName], savedData.orgs[orgName])
-      }
+      this.orgs.forEach((org, i) => {
+        this.orgs[i] = {
+          'name': org.name,
+          'metrics': Object.assign(this.orgs[i].metrics, savedData.orgs[i].metrics)
+        }
+      })
 
       this.global = Object.assign(this.global, savedData.global)
     }
 
     getJSON('https://data.heroku.com/dataclips/idfolumrbaubogbmewdoeyahhdtj.json', (data) => {
-      const queryName = 'Case Contact Count'
+      const metric = 'Case Contact Count'
 
       data.values.forEach((val) => {
-        let orgName
-
-        switch (val[0]) {
-          case 1:
-            orgName = 'Prince George'
-            break
-          case 2:
-            orgName = 'Montgomery'
-            break
-        }
-
-        if (orgName) {
-          this.orgs[orgName][queryName].newValue = val[1]
-        }
+        this.orgs[val[0] - 1].metrics[metric].newValue = val[1]
       })
 
-      this.queries[queryName] = 'loaded'
+      this.queries[metric] = 'loaded'
     })
 
     getJSON('https://data.heroku.com/dataclips/ymbdlyldhiiqcmsslbjfjdjmzwco.json', (data) => {
-      const queryName = 'Volunteers Assigned to Supervisors'
+      const metric = 'Volunteers Assigned to Supervisors'
 
       data.values.forEach((val) => {
-        let orgName
-
-        switch (val[0]) {
-          case 1:
-            orgName = 'Prince George'
-            break
-          case 2:
-            orgName = 'Montgomery'
-            break
-        }
-
-        if (orgName) {
-          this.orgs[orgName][queryName].newValue = val[1]
+        if (this.orgs[val[0] - 1]) {
+          this.orgs[val[0] - 1].metrics[metric].newValue = val[1]
         }
       })
 
-      this.queries[queryName] = 'loaded'
+      this.queries[metric] = 'loaded'
     })
 
     getJSON('https://data.heroku.com/dataclips/xsikhducnqfdrmfcntvdhtehuuwp.json', (data) => {
-      const queryName = 'Notification Count'
+      const metric = 'Notification Count'
 
       data.values.forEach((val) => {
-        let orgName
-
-        switch (val[0]) {
-          case 1:
-            orgName = 'Prince George'
-            break
-          case 2:
-            orgName = 'Montgomery'
-            break
+        if (this.orgs[val[0] - 1]) {
+          this.orgs[val[0] - 1].metrics[metric].newValue = val[1]
         }
-
-        this.orgs[orgName][queryName].newValue = val[1]
       })
 
-      this.queries[queryName] = 'loaded'
+      this.queries[metric] = 'loaded'
     })
 
     getJSON('https://data.heroku.com/dataclips/fairemyutljnkjgwldlaqtpecvvt.json', (data) => {
-      const queryName = 'Cases With Mandates'
+      const metric = 'Cases With Mandates'
 
       data.values.forEach((val) => {
-        let orgName
-
-        switch (val[0]) {
-          case 1:
-            orgName = 'Prince George'
-            break
-          case 2:
-            orgName = 'Montgomery'
-            break
+        if (this.orgs[val[0] - 1]) {
+          this.orgs[val[0] - 1].metrics[metric].newValue = val[1]
         }
-
-        this.orgs[orgName][queryName].newValue = val[1]
       })
 
-      this.queries[queryName] = 'loaded'
+      this.queries[metric] = 'loaded'
     })
 
     getJSON('https://data.heroku.com/dataclips/ibzctyhepsfsgpiobxrltuhejxds.json', (data) => {
       data.values.forEach((val) => {
-        let orgName, orgStat
+        let metric = val[1] ? 'Accepted Invitations' : 'Unaccepted Invitations'
 
-        switch (val[0]) {
-          case 1:
-            orgName = 'Prince George'
-            break
-          case 2:
-            orgName = 'Montgomery'
-            break
-        }
-
-        switch (val[1]) {
-          case true:
-            orgStat = 'Accepted Invitations'
-            break
-          case false:
-            orgStat = 'Unaccepted Invitations'
-            break
-        }
-
-        if (orgName) {
-          this.orgs[orgName][orgStat].newValue = val[2]
+        if (this.orgs[val[0] - 1]) {
+          this.orgs[val[0] - 1].metrics[metric].newValue = val[2]
         }
       })
 
@@ -241,31 +201,20 @@ const app = new Vue({
     })
 
     getJSON('https://data.heroku.com/dataclips/vgblwvzhclatsdxzdbihypqulckq.json', (data) => {
-      const queryName = 'Total Hours in Case Contacts'
+      const metric = 'Total Hours in Case Contacts'
 
       data.values.forEach((val) => {
-        this.global[queryName].newValue = Math.round(val[0])
+        this.global[metric].newValue = Math.round(val[0])
       })
 
-      this.queries[queryName] = 'loaded'
+      this.queries[metric] = 'loaded'
     })
 
     getJSON('https://data.heroku.com/dataclips/ahvopfhogmvuccdzdnncmwlioidd.json', (data) => {
       data.values.forEach((val) => {
-        let orgName
-
-        switch (val[0]) {
-          case 1:
-            orgName = 'Prince George'
-            break
-          case 2:
-            orgName = 'Montgomery'
-            break
-        }
-
-        if (orgName) {
-          this.orgs[orgName]['Users Who Have Added Case Contacts in Last 2 Weeks'].newValue = val[1]
-          this.orgs[orgName]['Case Contact Count in Last 2 Weeks'].newValue = val[2]
+        if (this.orgs[val[0] - 1]) {
+          this.orgs[val[0] - 1].metrics['Users Who Have Added Case Contacts in Last 2 Weeks'].newValue = val[1]
+          this.orgs[val[0] - 1].metrics['Case Contact Count in Last 2 Weeks'].newValue = val[2]
         }
       })
 
